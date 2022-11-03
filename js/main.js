4d1eab92e5e4c36a63c25eccb3a6025abb13be29
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-03 15:40:19 +0100

    adds changes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,27 @@
 
 const emailInput = prompt("Inserisci la tua mail");
 const emailList = ["violet.smith@email.com", "klaus.smith@email.com", "sunny.smith@email.com"];
+console.log(emailInput);
 console.log(emailList);
+
+let correctEmail = document.getElementById("correct-email");
+
+// controllo se email corrisponde e stampo messaggio esito 
+
+let result = false;
+
+for( let i = 0; i < emailList.length; i++){
+    if (emailInput === emailList[i]){
+        result = true;
+        console.log(emailList[i]);
+        console.log(correctEmail);
+    }
+
+}
+
+if ( result === true ) {
+    correctEmail.append("L'indirizzo email è corretto");
+
+} else {
+    correctEmail.append("L'indirizzo email non è corretto o non corrisponde a nessun utente");
+}
