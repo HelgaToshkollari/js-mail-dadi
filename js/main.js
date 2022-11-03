a2f5cf04e1ec6ab1237b87e292d283509a0e7309
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-03 15:20:25 +0100

    new file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//chiedo mail all'utente
+
+const emailInput = prompt("Inserisci la tua mail");
+const emailList = ["violet.smith@email.com", "klaus.smith@email.com", "sunny.smith@email.com"];
+console.log(emailList);
